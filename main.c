e6be386d3fec8c884090a049d9cee0ab50d906b9
José Guilherme Magalhães de Almeida <oficialjoseguilherme@gmail.com>
2025-10-13 04:50:19 -0300

    DEV main.c: desenvolvimento do fluxo de análise léxica

---
+++ main.c
@@ -0,0 +1,74 @@
+#include <stdlib.h>
+
+typedef struct cadeia {
+    char *str;
+    int length;
+} Cadeia;
+
+typedef struct analise {
+    int tamanho;
+    int aceite;
+} Analise;
+
+Analise analisador_lexico(Cadeia cadeia);
+Analise analise_lexica(Cadeia cadeia);
+Analise fluxo_lexico(Cadeia cadeia);
+
+int main(void) {
+
+    return 0;
+}
+
+Analise analisador_lexico(Cadeia cadeia) {
+    return analise_lexica(cadeia);
+}
+
+Analise analise_lexica(Cadeia cadeia) {
+    Analise analise;
+
+    analise = fluxo_lexico(cadeia);
+
+    return analise;
+}
+
+Analise fluxo_lexico(Cadeia cadeia) {
+    Analise analise = {
+        0,
+        0
+    };
+    
+    // Estado 0
+    switch (cadeia.str[analise.tamanho]) {
+        case '-': // Estado 1
+            analise.tamanho++;
+            int aceitou_digito_numerico = aceita_digito_numerico(cadeia.str[analise.tamanho]);
+            if (!aceitou_digito_numerico) {
+                return analise; // Parou no estado 1
+            }
+            analise.tamanho++; // Estado 2
+            break;
+        default: // Estado 0
+            int aceitou_digito_numerico = aceita_digito_numerico(cadeia.str[analise.tamanho]);
+            if (!aceitou_digito_numerico) { 
+                return analise; // Estado 0
+            }
+            analise.tamanho++; // Estado 2
+    }
+    analise.aceite = 1; // Estado 2
+
+    while (aceita_digito_numerico(cadeia.str[analise.tamanho])) analise.tamanho++; // Estado 2
+
+    if (cadeia.str[analise.tamanho] != ',') return analise; // Estado 5
+
+    if (!aceita_digito_numerico(cadeia.str[analise.tamanho])) return analise; // Estado 3
+
+    analise.tamanho += 2;
+
+    while (aceita_digito_numerico(cadeia.str[analise.tamanho])) analise.tamanho++;
+
+    return analise;
+}
+
+int aceita_digito_numerico(char c) {
+    return c >= '0' && c <= '9';
+}
